f19966e9b66f067035c32c02c969562d7ffb0481
pravesh00 <pravesh_c@me.iitr.ac.in>
2023-01-13 04:53:05 +0530

    seats event

--- app/src/main/java/com/example/ticketing/EventDetailActivity.kt
+++ app/src/main/java/com/example/ticketing/EventDetailActivity.kt
@@ -1,12 +1,17 @@
 package com.example.ticketing
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CheckBox
+import android.widget.LinearLayout
+import android.widget.TableLayout
+import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.bumptech.glide.Glide
 import com.example.ticketing.databinding.ActivityEventDetailBinding
 import com.example.ticketing.model.Event
+
 
 class EventDetailActivity : AppCompatActivity() {
     lateinit var binding:ActivityEventDetailBinding
@@ -15,11 +20,44 @@ class EventDetailActivity : AppCompatActivity() {
         binding= DataBindingUtil.setContentView(this, R.layout.activity_event_detail)
 
         var event = intent.getSerializableExtra("event") as Event
-        //Toast.makeText(this,event.name.toString(),Toast.LENGTH_SHORT).show()
         Glide.with(this).load(event.photo).centerCrop().into(binding.imgPhoto)
         binding.txtDesc.setText(event.description)
         binding.txtName.setText(event.name)
         binding.txtType.setText(event.typeEvent)
+        var linearLayout = binding.lnrTable
+        var m = Array(10) {Array(15) {0} }
+        var totalSeats:Int = 0
+
+        for(i in 0..9){
+            var newlinear = LinearLayout(this)
+
+            for( j in 0..14){
+                var chk = CheckBox(this)
+                chk.setLayoutParams(
+                    LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.WRAP_CONTENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT, 1f
+                    )
+                )
+                chk.setOnClickListener {
+                    if(m[i][j]===2){
+                        m[i][j]=0
+                        totalSeats-=1
+                    }else {
+                        m[i][j]=2
+                        totalSeats+=1
+                    }
+                    binding.txtTickets.text = "Tickets : "+totalSeats.toString()
+                }
+                if(m[i][j]===1){
+                    chk.isChecked= true
+                    chk.isEnabled=false
+                }
+                newlinear.addView(chk)
+            }
+            linearLayout.addView(newlinear)
+        }
+
 
     }
 }
